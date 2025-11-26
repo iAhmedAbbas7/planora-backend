49645613b0c225d6365a01225a4bb12b6e4a1bb2
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-26 10:36:13 +0500

    Add Email Change Routes for Verification and Cancellation. Implemented Endpoints for Sending and Verifying Email Change Codes, as well as Canceling Email Change Requests, enhancing User Account Management Functionality

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -4,6 +4,13 @@ import {
   updateAccount,
   deleteAccount,
 } from "../controllers/account.controller.js";
+import {
+  sendCurrentEmailCode,
+  verifyCurrentEmailCode,
+  verifyNewEmailCode,
+  resendVerificationCode,
+  cancelEmailChange,
+} from "../controllers/emailChange.controller.js";
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
@@ -19,5 +26,15 @@ router.get("/info", getAccount);
 router.put("/update", updateAccount);
 // DELETE ACCOUNT
 router.delete("/delete", deleteAccount);
+// CANCEL EMAIL CHANGE
+router.delete("/email/cancel", cancelEmailChange);
+// VERIFY NEW EMAIL CODE AND UPDATE EMAIL
+router.post("/email/verify-new-code", verifyNewEmailCode);
+// RESEND VERIFICATION CODE
+router.post("/email/resend-code", resendVerificationCode);
+// SEND VERIFICATION CODE TO CURRENT EMAIL
+router.post("/email/verify-current", sendCurrentEmailCode);
+// VERIFY CURRENT EMAIL CODE
+router.post("/email/verify-current-code", verifyCurrentEmailCode);
 
 export default router;
