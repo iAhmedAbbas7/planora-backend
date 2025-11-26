ea3eca66256d7b2e2d691f7bfbedbb099ccea274
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-26 11:23:24 +0500

    Add Password Change Routes. Implemented new Endpoints for Changing, Verifying, and Canceling Password Change Requests, enhancing User Account Management and Security Features

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -11,6 +11,13 @@ import {
   resendVerificationCode,
   cancelEmailChange,
 } from "../controllers/emailChange.controller.js";
+import {
+  sendPasswordChangeCode,
+  verifyPasswordChangeCode,
+  changePassword,
+  resendPasswordChangeCode,
+  cancelPasswordChange,
+} from "../controllers/passwordChange.controller.js";
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
@@ -26,14 +33,24 @@ router.get("/info", getAccount);
 router.put("/update", updateAccount);
 // DELETE ACCOUNT
 router.delete("/delete", deleteAccount);
+// CHANGE PASSWORD
+router.post("/password/change", changePassword);
 // CANCEL EMAIL CHANGE
 router.delete("/email/cancel", cancelEmailChange);
+// CANCEL PASSWORD CHANGE
+router.delete("/password/cancel", cancelPasswordChange);
 // VERIFY NEW EMAIL CODE AND UPDATE EMAIL
 router.post("/email/verify-new-code", verifyNewEmailCode);
 // RESEND VERIFICATION CODE
 router.post("/email/resend-code", resendVerificationCode);
 // SEND VERIFICATION CODE TO CURRENT EMAIL
 router.post("/email/verify-current", sendCurrentEmailCode);
+// SEND PASSWORD CHANGE CODE
+router.post("/password/send-code", sendPasswordChangeCode);
+// VERIFY PASSWORD CHANGE CODE
+router.post("/password/verify-code", verifyPasswordChangeCode);
+// RESEND PASSWORD CHANGE CODE
+router.post("/password/resend-code", resendPasswordChangeCode);
 // VERIFY CURRENT EMAIL CODE
 router.post("/email/verify-current-code", verifyCurrentEmailCode);
 
