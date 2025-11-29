71185b59eb54aca2f70015289d17f28da18258cd
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-29 10:31:36 +0500

    Add Recovery Email Management Routes. Introduced new Routes for Requesting, Verifying, and Canceling Recovery Email Processes, Enhancing User Support for Account Recovery and improving Overall Authentication Workflow

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -24,6 +24,17 @@ import {
   resendDeletionCode,
   cancelAccountDeletion,
 } from "../controllers/accountDeletion.controller.js";
+import {
+  requestAddRecoveryEmail,
+  verifyAddRecoveryEmail,
+  requestUpdateRecoveryEmail,
+  verifyUpdateRecoveryEmailCurrent,
+  verifyUpdateRecoveryEmailNew,
+  requestRemoveRecoveryEmail,
+  verifyRemoveRecoveryEmail,
+  resendRecoveryEmailCode,
+  cancelRecoveryEmail,
+} from "../controllers/recoveryEmail.controller.js";
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
@@ -33,6 +44,16 @@ const router = express.Router();
 // <== ROUTES ==>
 // ALL ROUTES REQUIRE AUTHENTICATION
 router.use(isAuthenticated);
+// VERIFY NEW RECOVERY EMAIL CODE AND UPDATE
+router.post(
+  "/recovery-email/update/verify-new-code",
+  verifyUpdateRecoveryEmailNew
+);
+// VERIFY CURRENT RECOVERY EMAIL CODE
+router.post(
+  "/recovery-email/update/verify-current-code",
+  verifyUpdateRecoveryEmailCurrent
+);
 // GET ACCOUNT INFO
 router.get("/info", getAccount);
 // UPDATE ACCOUNT
@@ -61,11 +82,25 @@ router.post("/email/resend-code", resendVerificationCode);
 router.post("/email/verify-current", sendCurrentEmailCode);
 // SEND PASSWORD CHANGE CODE
 router.post("/password/send-code", sendPasswordChangeCode);
+// CANCEL RECOVERY EMAIL PROCESS
+router.delete("/recovery-email/cancel", cancelRecoveryEmail);
 // VERIFY PASSWORD CHANGE CODE
 router.post("/password/verify-code", verifyPasswordChangeCode);
 // RESEND PASSWORD CHANGE CODE
 router.post("/password/resend-code", resendPasswordChangeCode);
 // VERIFY CURRENT EMAIL CODE
 router.post("/email/verify-current-code", verifyCurrentEmailCode);
+// RESEND RECOVERY EMAIL CODE
+router.post("/recovery-email/resend-code", resendRecoveryEmailCode);
+// VERIFY CODE AND ADD RECOVERY EMAIL
+router.post("/recovery-email/add/verify-code", verifyAddRecoveryEmail);
+// REQUEST TO ADD RECOVERY EMAIL
+router.post("/recovery-email/add/request-code", requestAddRecoveryEmail);
+// VERIFY CODE AND REMOVE RECOVERY EMAIL
+router.post("/recovery-email/remove/verify-code", verifyRemoveRecoveryEmail);
+// REQUEST TO UPDATE RECOVERY EMAIL
+router.post("/recovery-email/update/request-code", requestUpdateRecoveryEmail);
+// REQUEST TO REMOVE RECOVERY EMAIL
+router.post("/recovery-email/remove/request-code", requestRemoveRecoveryEmail);
 
 export default router;
